3b7b06df124dc990d9b0318656107290d8e8cb0e
Braja Khaslana <aryachakabrajawisesa@gmail.com>
2026-01-08 14:58:37 +0700

    hai

--- src/App.jsx
+++ src/App.jsx
@@ -1,3 +1,4 @@
+import Hero from './components/Hero'
 import Navbar from './components/Navbar'
 import './styles/index.css'
 
@@ -6,10 +7,7 @@ function App() {
   return (
    <div className='App'>
     <Navbar/>
-    <main style={{ padding:"50px", textAlign: "center", fontSize: "0.8rem"}}>
-    <h1>Hello, Saya Arya Chaka B.W.</h1>
-    <p>Salam kenal, Saya hanya programmer biasa</p>
-    </main>
+    <Hero/>
    </div>
   );
 }
